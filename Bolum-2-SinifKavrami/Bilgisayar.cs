190998e9820e802d7395c30bfc1c35c2c78623af
info@yazilimfikirleri.com <info@yazilimfikirleri.com>
2020-06-23 13:16:30 +0300

    proje oluşturuldu.

---
+++ Bolum-2-SinifKavrami/Bilgisayar.cs
@@ -0,0 +1,34 @@
+﻿
+
+namespace Bolum_2_SinifKavrami
+{
+    class Bilgisayar
+    {
+        //Bir bilgisayarın ..... sı olur.
+
+        //field
+       //public string marka;
+       //public string model;
+       //public double ekranBoyut;
+       //public string ekranKarti;
+       //public string isletimSistemi;
+       //public string islemci;
+       //public int ram;
+       //public int hdKapasite;
+       //public decimal fiyat;
+
+        //Property
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public double EkranBoyutu { get; set; }
+        public string EkranKarti { get; set; }
+        public string IsletimSistemi { get; set; }
+        public string Islemci { get; set; }
+        public int Ram { get; set; }
+        public int HdKapasite { get; set; }
+        public decimal Fiyat { get; set; }
+
+
+
+    }
+}
---
+++ Bolum-2-SinifKavrami/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bolum_2_SinifKavrami
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Örnek (Instance) alma.
+            Bilgisayar laptop = new Bilgisayar();
+            //laptop.marka = "Dell";
+            //laptop.model = "Xps";
+            //laptop.ekranBoyut = 15.6;
+            //laptop.isletimSistemi = "Windows";
+            //laptop.islemci = "i7";
+            //laptop.ram = 16;
+            //laptop.hdKapasite = 1000;
+            //laptop.fiyat = 10000;
+
+            //MessageBox.Show(laptop.marka);
+
+            laptop.Marka = "Dell";
+            laptop.Model = "Xps";
+
+            MessageBox.Show(laptop.Marka);
+        }
+    }
+}
